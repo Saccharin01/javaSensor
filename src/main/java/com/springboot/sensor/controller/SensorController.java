1a2cb5e629689970d7fe9450c30404f5fb4df725
saccharin01 <chousik01@gmail.com>
2025-04-23 12:44:58 +0900

    new | SensorController.java
    센서 정보를 담당하는 엔드포인트 컨트롤러 스켈레톤 정의

---
+++ src/main/java/com/springboot/sensor/controller/SensorController.java
@@ -0,0 +1,13 @@
+package com.springboot.sensor.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/sensor")
+public class SensorController {
+
+}
