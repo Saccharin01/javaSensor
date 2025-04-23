48d0949cfe4b83d87b55ca7c06ba3c3cca609e29
saccharin01 <chousik01@gmail.com>
2025-04-23 15:48:53 +0900

    feat | SensorController.java
    스켈레톤 컨트롤러에 포스트 메서드 매핑

--- src/main/java/com/springboot/sensor/controller/SensorController.java
+++ src/main/java/com/springboot/sensor/controller/SensorController.java
@@ -1,13 +1,35 @@
 package com.springboot.sensor.controller;
 
+import com.springboot.sensor.data.dto.SensorRequestDTO;
+import com.springboot.sensor.service.SensorService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/sensor")
 public class SensorController {
+    private final SensorService sensorService;
 
+
+    @Autowired
+    public SensorController(SensorService sensorService) {
+        this.sensorService = sensorService;
+    }
+
+
+
+    @Operation(
+            summary = "test",
+            description = "test"
+    )
+    @PostMapping
+    public ResponseEntity<SensorRequestDTO> getData(@RequestBody SensorRequestDTO requestDTO) {
+        SensorRequestDTO sensorData = sensorService.postSensorData(requestDTO); //
+
+        return ResponseEntity.status(HttpStatus.OK).body(sensorData);
+
+    }
 }
