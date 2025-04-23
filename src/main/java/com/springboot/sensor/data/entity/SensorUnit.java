fc722281c50bd6a8c48c724bdad4085d0c36d544
saccharin01 <chousik01@gmail.com>
2025-04-23 11:12:53 +0900

    new | SensorUnit.java
    센서 유닛 엔티티 생성

---
+++ src/main/java/com/springboot/sensor/data/entity/SensorUnit.java
@@ -0,0 +1,32 @@
+package com.springboot.sensor.data.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "sensor_unit")
+public class SensorUnit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long number;
+
+    @Column(nullable = false, unique = true)
+    private String chip_id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String location;
+
+    @OneToMany(mappedBy = "sensorUnit", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<SensorData> dataList = new ArrayList<>();
+
+}
