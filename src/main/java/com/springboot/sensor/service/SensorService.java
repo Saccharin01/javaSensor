2ade85645a8f398ef8e7ef1ad2ba8c3fdc355775
saccharin01 <chousik01@gmail.com>
2025-04-23 14:31:15 +0900

    new | SensorService.java
    서비스 레이어 스켈레톤 인터페이스 선언

---
+++ src/main/java/com/springboot/sensor/service/SensorService.java
@@ -0,0 +1,13 @@
+package com.springboot.sensor.service;
+
+import com.springboot.sensor.data.dto.SensorRequestDTO;
+import java.util.List;
+
+
+public interface SensorService {
+
+    SensorRequestDTO postSensorData(); // 아두이노 보드에서 백엔드로 데이터를 전송할 때
+
+    List<SensorRequestDTO> getSenseorData(); // 해당 결과값을 바탕으로 ApexChart 에서 차트를 그려야 함
+
+}
