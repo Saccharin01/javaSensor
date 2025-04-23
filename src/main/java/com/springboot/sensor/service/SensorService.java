3c48ff90de9f96da04e9cda0f125895d1cf9d709
saccharin01 <chousik01@gmail.com>
2025-04-23 15:44:24 +0900

    feat | SensorService.java
    서비스 레이어 인터페이스에 선언한 메서드 중 PostSensorData 메서드에 인자 전달 부분 누락 수정

--- src/main/java/com/springboot/sensor/service/SensorService.java
+++ src/main/java/com/springboot/sensor/service/SensorService.java
@@ -2,11 +2,12 @@ package com.springboot.sensor.service;
 
 import com.springboot.sensor.data.dto.SensorRequestDTO;
 import java.util.List;
+import com.springboot.sensor.data.dto.SensorRequestDTO;
 
 
 public interface SensorService {
 
-    SensorRequestDTO postSensorData(); // 아두이노 보드에서 백엔드로 데이터를 전송할 때
+    SensorRequestDTO postSensorData(SensorRequestDTO data); // 아두이노 보드에서 백엔드로 데이터를 전송할 때
 
     List<SensorRequestDTO> getSenseorData(); // 해당 결과값을 바탕으로 ApexChart 에서 차트를 그려야 함
 
