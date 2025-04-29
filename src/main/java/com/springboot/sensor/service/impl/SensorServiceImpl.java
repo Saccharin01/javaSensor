30545f7306f3b03de3016d001274e1f6810b29cc
saccharin01 <chousik01@gmail.com>
2025-04-30 07:24:12 +0900

    feat | SensorServiceImpl.java
    서비스 구현체에서 데이터베이스를 핸들링 하던 부분 삭제. 해당 부분은 리포지트리 레이어로 이관
    
    또한 트렌젝션 처리를 서비스 구현체에서 책임

--- src/main/java/com/springboot/sensor/service/impl/SensorServiceImpl.java
+++ src/main/java/com/springboot/sensor/service/impl/SensorServiceImpl.java
@@ -1,12 +1,11 @@
 package com.springboot.sensor.service.impl;
 
 import com.springboot.sensor.data.dto.SensorResponseDTO;
-import com.springboot.sensor.data.entity.SensorData;
 import com.springboot.sensor.data.entity.SensorUnit;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.springboot.sensor.service.SensorService;
-import com.springboot.sensor.data.repository.SensorDataRepository;
 import com.springboot.sensor.data.repository.SensorUnitRepository;
 import com.springboot.sensor.data.dto.SensorRequestDTO;
 
@@ -17,49 +16,38 @@ import java.util.List;
 @Service
 public class SensorServiceImpl implements SensorService {
 
-    private final SensorDataRepository sensorDataRepository;
     private final SensorUnitRepository sensorUnitRepository;
 
     @Autowired
     public SensorServiceImpl(
-            SensorDataRepository sensorDataRepository,
             SensorUnitRepository sensorUnitRepository)
     {
-        this.sensorDataRepository = sensorDataRepository;
         this.sensorUnitRepository = sensorUnitRepository;
     }
 
     @Override
+    @Transactional
     public SensorRequestDTO postSensorData(SensorRequestDTO data) {
-
-        // 1. chip_id로 센서 유닛 조회
-        SensorUnit unit = sensorUnitRepository.findByChipId(data.getChipId())
-                .orElseGet(() -> {
-                    SensorUnit newUnit = new SensorUnit();
-                    newUnit.setChipId(data.getChipId());
-                    newUnit.setName(data.getName());
-                    newUnit.setLocation(data.getLocation());
-                    return sensorUnitRepository.save(newUnit);
-                });
-
-        // 2. 시간 검증 또는 서버 시간으로 대체
+        // 1. 시간 검증
         LocalDateTime timestamp = data.getSensedTime();
         if (timestamp == null || timestamp.getYear() < 2001 || timestamp.getYear() > 2030) {
             timestamp = LocalDateTime.now();
         }
 
-        // 3. 센서 데이터 저장부
-        SensorData sensorData = new SensorData();
-        sensorData.setSensorUnit(unit); // 연관관계
-        sensorData.setSensedData(data.getSensedData());
-        sensorData.setSensedTime(timestamp);
+        // 2. 유닛 조회 및 없으면 생성
+        SensorUnit unit = sensorUnitRepository.findOrCreateSensorUnit(
+                data.getChipId(), data.getName(), data.getLocation());
 
-        sensorDataRepository.save(sensorData);
+        // 3. 데이터 저장
+        sensorUnitRepository.saveSensorData(unit, data.getSensedData(), timestamp);
 
-        // 4. 요청 완료 시 확인을 위해 데이터 반환과 data 객체에 서버에서 부여한 시간 추가
+        // 4. 반환 데이터 세팅
         data.setSensedTime(timestamp);
         return data;
     }
+
+
+
     public List<String> getSensorUnitIds() {
         return sensorUnitRepository.getSensorUnitIds();
     }
