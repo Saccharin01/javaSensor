b81da5945c37082eac45c5cf39acda866a3cbbd6
saccharin01 <chousik01@gmail.com>
2025-04-29 23:45:05 +0900

    feat | SensorResponseDTO.java
    클라이언트에 응답하는 데이터 형태 정의. SensorDataDTO 클래스를 이용해 data 형식을 지정

---
+++ src/main/java/com/springboot/sensor/data/dto/SensorResponseDTO.java
@@ -0,0 +1,23 @@
+package com.springboot.sensor.data.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class SensorResponseDTO {
+
+    private String chipId;               // 센서 식별자
+    private String name;                 // 센서 이름
+    private String location;             // 설치 장소
+    private List<SensorDataDTO> data;     // 측정 데이터 목록
+
+    public SensorResponseDTO(String chipId, String name, String location, List<SensorDataDTO> data) {
+        this.chipId = chipId;
+        this.name = name;
+        this.location = location;
+        this.data = data;
+    }
+}
