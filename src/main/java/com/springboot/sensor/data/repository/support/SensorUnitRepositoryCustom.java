c3e0766d0d0f8c0aa762b32f79fb278779c432de
saccharin01 <chousik01@gmail.com>
2025-05-12 11:24:12 +0900

    feat | SensorUnitRepositoryCustom.java
    커스텀 인터페이스 센서 유닛 리포지트리 인터페이스에 새로운 메서드 추가

--- src/main/java/com/springboot/sensor/data/repository/support/SensorUnitRepositoryCustom.java
+++ src/main/java/com/springboot/sensor/data/repository/support/SensorUnitRepositoryCustom.java
@@ -7,8 +7,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface SensorUnitRepositoryCustom {
-//    Optional<SensorUnit> findUnitByChipIdWithQueryDsl(String chipId);
-
     SensorUnit findOrCreateSensorUnit(String chipId, String name, String location);
 
     void saveSensorData(SensorUnit unit, int sensedData, LocalDateTime sensedTime);
@@ -17,5 +15,9 @@ public interface SensorUnitRepositoryCustom {
 
     SensorResponseDTO findSensorUnitWithDataByChipId(String chipId);
 
+    //유닛 선택 시 기본적으로 가장 최신 데이터에서 50개만 잘라서 넘겨주는 메서드
     SensorResponseDTO findSensorDataLimit(String chipId, int limit);
+
+    //사용자의 요구에 따라 검색 기능을 수행하는 메서드
+    SensorResponseDTO findSensorDataByConditions(String chipId, String type, String selectedDateStr, String direction, Integer count);
 }
