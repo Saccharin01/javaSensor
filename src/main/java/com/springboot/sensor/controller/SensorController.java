d3b1c51c5d936f95ddee530f6eea5f6599315604
saccharin01 <chousik01@gmail.com>
2025-05-12 11:26:01 +0900

    feat | SensorController.java
    엔드포인트를 더 직관적으로 나눌 수 있도록 수정
    
    검색 옵션에 대한 메서드 실행 결과를 컨트롤 할 수 있도록 get 메서드 매핑 추가

--- src/main/java/com/springboot/sensor/controller/SensorController.java
+++ src/main/java/com/springboot/sensor/controller/SensorController.java
@@ -29,7 +29,7 @@ public class SensorController {
             summary = "센서 데이터를 전송하는 엔드포인트",
             description = "센서에서 측정된 데이터를 데이터베이스에 전송하는 메서드입니다"
     )
-    @PostMapping
+    @PostMapping(value = "/data")
     public ResponseEntity<SensorRequestDTO> getData(@RequestBody SensorRequestDTO requestDTO) {
         SensorRequestDTO sensorData = sensorService.postSensorData(requestDTO); //
 
@@ -41,7 +41,7 @@ public class SensorController {
             summary = "센서 유닛의 id 값을 가져오는 메서드입니다.",
             description = "테이블에 저장되어있는 센서의 id를 chipIds 키에 매핑하여 반환합니다."
     )
-    @GetMapping
+    @GetMapping( "/units")
     public ResponseEntity<SensorUnitIdDTO> getSensorUnitId() {
         List<String> chipIds = sensorService.getSensorUnitIds();
 
@@ -52,9 +52,22 @@ public class SensorController {
             summary = "특정 센서 유닛의 데이터를 가져오는 엔드포인트",
             description = "쿼리스트링 chipId를 기반으로 센서 데이터를 조회합니다."
     )
-    @GetMapping("/data")
-    public ResponseEntity<SensorResponseDTO> getSensorData(@RequestParam String chipId) {
+    @GetMapping("/units/basic")
+    public ResponseEntity<SensorResponseDTO> getSensorDataBasic(@RequestParam String chipId) {
         SensorResponseDTO response = sensorService.getSensorDataLimit(chipId);
+        return ResponseEntity.ok(response);
+    }
+
+
+    @GetMapping("/units/search")
+    public ResponseEntity<SensorResponseDTO> getSensorDataSearched (
+            @RequestParam String chipId,
+            @RequestParam String type,
+            @RequestParam String selectedDate,
+            @RequestParam(required = false) String direction,
+            @RequestParam(required = false) Integer count
+    ){
+        SensorResponseDTO response = sensorService.getSensorDataWithQuery(chipId, type, selectedDate, direction, count);
         return ResponseEntity.ok(response);
     }
 }
