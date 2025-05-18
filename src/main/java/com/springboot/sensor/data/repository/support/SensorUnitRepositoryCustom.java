8d58f24ecbef42bbc6fa87e56935346f8397f639
saccharin01 <chousik01@gmail.com>
2025-05-18 20:42:55 +0900

    feat | SensorUnitRepositoryCustom.java
    검색 기능 구현에 따른 리포지트리 인터페이스 선언

--- src/main/java/com/springboot/sensor/data/repository/support/SensorUnitRepositoryCustom.java
+++ src/main/java/com/springboot/sensor/data/repository/support/SensorUnitRepositoryCustom.java
@@ -1,5 +1,6 @@
 package com.springboot.sensor.data.repository.support;
 
+import com.springboot.sensor.data.dto.AggregatedDataDTO;
 import com.springboot.sensor.data.dto.SensorResponseDTO;
 import com.springboot.sensor.data.entity.SensorUnit;
 
@@ -20,4 +21,19 @@ public interface SensorUnitRepositoryCustom {
 
     //사용자의 요구에 따라 검색 기능을 수행하는 메서드
     SensorResponseDTO findSensorDataByConditions(String chipId, String type, String selectedDateStr, String direction, Integer count);
+
+    //연도 검색 시 해당 년도의 각 월별 평균치를 가져옴
+    List<AggregatedDataDTO> getMonthlyAveragesByYear(String chipId, int year);
+
+    //월별 검색 시 각 해당 월차의 각 주차별 평균치를 가져옴
+    List<AggregatedDataDTO> getWeeklyAvgByMonth(String chipId, int year, int month);
+
+    //일차 별 검색 시 해당 일차의 시간 별 평균치를 가져옴
+    List<AggregatedDataDTO> getHourlyAvgByDay(String chipId, int year, int month, int day);
+
+    //주차 별 검색 시 해당 주차의 각 일별 평균치
+    List<AggregatedDataDTO> getDailyAvgByWeek(String chipId, int year, int month, int week);
+
+    //시간 검색에 해당하는 메서드
+    List<AggregatedDataDTO> getRawDataByHour(String chipId, LocalDateTime dateTime);
 }
